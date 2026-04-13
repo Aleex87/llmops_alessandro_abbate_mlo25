d14197878df0b6e063598a228805c7b859b30be3
Aleex87 <alessandrodanteabbate@gmail.com>
2026-04-13 13:43:37 +0200

    build a streamlit frontend connecte to the api lection 06

--- 06_pydanticai_fastapi_chatbot/chat_agent.py
+++ 06_pydanticai_fastapi_chatbot/chat_agent.py
@@ -12,5 +12,5 @@ agent = Agent(model=MODEL_SMALL,
 
 async def chat(request: ChatRequest):
     result= await agent.run(request.question, message_history=request.message_history)
-    return ChatResponse(response=result.output, message_history=result.all_messages)
+    return ChatResponse(response=result.output, message_history=result.all_messages())
     
